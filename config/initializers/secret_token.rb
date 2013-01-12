5268940b4e035a001463711261d8a451b060d4df
The Crimson Tautology <billehs+github@gmail.com>
2013-01-12 12:03:55 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f0f4919df57e9feb3fa74de27dab91fe58a7c392eb6c7aabff07562302528957623e86646a8ec9ce3f24d275c6618c11da76a70d045e1e6a12c9c72d03837c3c'
